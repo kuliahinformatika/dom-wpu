3bd33e81348b2d2e25ad9fb242497c8804b46710
kuliahinformatika <murodalhakim01@gmail.com>
2025-09-30 17:48:53 +0700

    DOM Selection (Part 1) = documentByClassName

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ const p = document.getElementsByTagName('p')
 for (let i = 0; i < p.length; i ++) {
 p[i].style.backgroundColor = 'lightblue'
 }
+
+const h1 = document.getElementsByTagName('h1')[0]
+h1.style.fontSize = '50px'
+
+
+//dokument.getElementsByClassName()
+//==>HTMLCollection
+
+const p1 = document.getElementsByClassName('p1')
+p1[0].innerHTML = 'hallo bos iki diganti'
