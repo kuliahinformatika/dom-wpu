ec03368a53827b2fc2a847019497b7a56c421dd8
kuliahinformatika <murodalhakim01@gmail.com>
2025-10-02 20:10:39 +0700

    innerHTML, setAtribute,classList

---
+++ dommanipulation1.js
@@ -0,0 +1,26 @@
+//DOM Manipulation Method
+
+//innerHTML
+
+// const judul = document.getElementById('judul')
+// judul.innerHTML = "hakim"
+
+// const sectionA = document.querySelector('section#a')
+// sectionA.innerHTML = "hello world"
+
+// const judul =document.querySelector('#judul')
+// judul.style.color = 'blue'
+// judul.style.backgroundColor = 'lightblue'
+
+//setAttribute
+
+// const judul =document.getElementsByTagName('h1')[0]
+// judul.setAttribute('name', 'hakim')
+
+// const a = document.querySelector('section#a a')
+// a.setAttribute('name', 'hakim')
+
+//classList
+const p2 = document.querySelector('.p2')
+// p2.classList.add('label')
+// p2.classList.remove('label')
